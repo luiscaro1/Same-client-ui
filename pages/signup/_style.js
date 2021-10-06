3377c4c2043daddf89b2c34ee4573816c8194faf
JJMP127 <jovan.mangual@upr.edu>
2021-10-05 21:55:07 -0300

    Added Signup page.

---
+++ pages/signup/_style.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  red: {
+
+    color:'red',
+    fontSize: 20
+  },
+  blue:{
+    color:'blue',
+    fontSize:30
+  }
+});
+
+export default useStyles
---
+++ pages/signup/index.js
@@ -0,0 +1,83 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import { TextField } from "@mui/material";
+import FormGroup from "@mui/material/FormGroup";
+import { useDispatch } from "react-redux";
+import { authActions } from "../../services/redux/store/actions";
+import { useStyles } from "./_style";
+
+// react component
+const Signup = () => {
+  // applies styling to components
+  // const classes = useStyles();
+
+  // function to dispatch events
+  const dispatch = useDispatch();
+
+  // hold the values of the text fields
+  const [values, setValues] = React.useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    uausername: "",
+    uapassword: "",
+  });
+
+  console.log(values);
+
+  // maps textfield values to state values
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // submits the values
+  const handleSumbit = (e) => {
+    e.preventDefault();
+
+    dispatch(authActions.login(values));
+  };
+
+  return (
+    <form onSubmit={handleSumbit}>
+      <FormGroup>
+      <TextField
+          name="firstname"
+          required
+          label="firstname"
+          onChange={handleChange}
+        />
+        <TextField
+          name="lastname"
+          required
+          label="lastname"
+          onChange={handleChange}
+        />
+        <TextField
+          name="email"
+          required
+          label="email"
+          onChange={handleChange}
+        />
+        <TextField
+          name="uausername"
+          required
+          label="username"
+          onChange={handleChange}
+        />
+        <TextField
+          name="uapassword"
+          type="password"
+          required
+          label="password"
+          onChange={handleChange}
+        />
+        <Button type="submit">Register</Button>
+      </FormGroup>
+    </form>
+  );
+};
+
+export default Signup;

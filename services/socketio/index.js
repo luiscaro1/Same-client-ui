0b003b6899818bdf5d4291b61f03636b06ce71ed
Luis <luis.caro1@upr.edu>
2021-10-20 21:54:47 -0500

    fixing game and chat  urls

--- services/socketio/index.js
+++ services/socketio/index.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const lobbySocket = io(process.env.CHAT_URL);
-const gameSocket = io(process.env.GAME_URL);
+const lobbySocket = io(process.env.NEXT_PUBLIC_CHAT_URL);
+const gameSocket = io(process.env.NEXT_PUBLIC_GAME_URL);
 
 export { lobbySocket, gameSocket };

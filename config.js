320b5db2e772d310f789fa5b76b4538606ca09e1
Luis <luis.caro1@upr.edu>
2021-11-11 15:37:01 -0600

    updating api routes

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
   auth_api: {
     base_url: process.env.NEXT_PUBLIC_AUTH_URL,
-    login_route: "/auth/login",
-    signup_route: "/auth/signup",
-    verify_auth: "/auth/verify",
+    login_route: "/Same/login",
+    signup_route: "/Same/signup",
+    verify_auth: "/Same/accounts/getCookieOwner",
   },
   game_api: {
     base_url: process.env.NEXT_PUBLIC_GAME_URL,
